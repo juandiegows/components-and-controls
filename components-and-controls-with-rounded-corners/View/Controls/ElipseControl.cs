503f79493327ed317c1de764d7fc34d5addd8246
Juan Diego Mejía Maestre <Mejiamaestrejuandiego@gmail.com>
2021-05-03 22:40:34 -0500

    mover paneles interno

---
+++ components-and-controls-with-rounded-corners/View/Controls/ElipseControl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace View.Components
+{
+    class ElipseControl : Control
+    {
+
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+           (
+              int nLeftRect,
+              int nTopRect,
+              int nRightRect,
+              int nBottomRect,
+              int nWidthEllipse,
+              int nHeightEllipse
+           );
+
+        [DllImport("Gdi32.dll", EntryPoint = "DeleteObject")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+
+        private void RecreateRegion()
+        {
+            IntPtr handle = CreateRoundRectRgn(0, 0, Width, Height, _CornerRadius, _CornerRadius);
+            Region = Region.FromHrgn(handle);
+            DeleteObject(handle);
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            this.RecreateRegion();
+        }
+
+        private int _CornerRadius = 30;
+
+        public int CornerRadius
+        {
+            get { return _CornerRadius; }
+            set
+            {
+                _CornerRadius = value;
+                this.RecreateRegion();
+            }
+        }
+    }
+}
--- components-and-controls-with-rounded-corners/View/FormExample.Designer.cs
+++ components-and-controls-with-rounded-corners/View/FormExample.Designer.cs
@@ -29,7 +29,13 @@
         private void InitializeComponent()
         {
             this.elipseComponentFormExample = new View.Components.ElipseComponent();
-            this.moveFormCompomenteFormExample = new View.Components.MoveFormCompomente();
+            this.moveCompomenteFormExample = new View.Components.MoveCompomente();
+            this.label1 = new System.Windows.Forms.Label();
+            this.elipseControl1 = new View.Components.ElipseControl();
+            this.elipseControl2 = new View.Components.ElipseControl();
+            this.moveCompomente1 = new View.Components.MoveCompomente();
+            this.moveCompomente2 = new View.Components.MoveCompomente();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // elipseComponentFormExample
@@ -37,16 +43,70 @@
             this.elipseComponentFormExample.CornerRadius = 40;
             this.elipseComponentFormExample.TargetControl = this;
             // 
-            // moveFormCompomenteFormExample
+            // moveCompomenteFormExample
             // 
-            this.moveFormCompomenteFormExample.TargetControl = this;
+            this.moveCompomenteFormExample.TargetControl = this;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(161, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(472, 94);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido al Formulario con esquina redondeada";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // elipseControl1
+            // 
+            this.elipseControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.elipseControl1.CornerRadius = 50;
+            this.elipseControl1.Location = new System.Drawing.Point(295, 166);
+            this.elipseControl1.Name = "elipseControl1";
+            this.elipseControl1.Size = new System.Drawing.Size(140, 92);
+            this.elipseControl1.TabIndex = 1;
+            this.elipseControl1.Text = "elipseControl1";
+            // 
+            // elipseControl2
+            // 
+            this.elipseControl2.BackColor = System.Drawing.Color.Blue;
+            this.elipseControl2.CornerRadius = 50;
+            this.elipseControl2.Location = new System.Drawing.Point(393, 208);
+            this.elipseControl2.Name = "elipseControl2";
+            this.elipseControl2.Size = new System.Drawing.Size(144, 95);
+            this.elipseControl2.TabIndex = 2;
+            this.elipseControl2.Text = "elipseControl2";
+            // 
+            // moveCompomente1
+            // 
+            this.moveCompomente1.TargetControl = this.elipseControl1;
+            // 
+            // moveCompomente2
+            // 
+            this.moveCompomente2.TargetControl = this.elipseControl2;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(257, 342);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(280, 40);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "intenta mover los rectangulos";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormExample
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(125)))), ((int)(((byte)(12)))));
+            this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.elipseControl2);
+            this.Controls.Add(this.elipseControl1);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormExample";
             this.Text = "Form Example";
@@ -57,7 +117,13 @@
         #endregion
 
         private Components.ElipseComponent elipseComponentFormExample;
-        private Components.MoveFormCompomente moveFormCompomenteFormExample;
+        private Components.MoveCompomente moveCompomenteFormExample;
+        private System.Windows.Forms.Label label1;
+        private Components.ElipseControl elipseControl1;
+        private Components.ElipseControl elipseControl2;
+        private Components.MoveCompomente moveCompomente1;
+        private Components.MoveCompomente moveCompomente2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
